3da16ff687f344e267741930ec6beb7e24b4f592
ArmorBoyd <boydpoblete@gmail.com>
2023-10-24 23:16:16 +0800

    Final

--- Final Project/w07-task.js
+++ Final Project/w07-task.js
@@ -1,104 +1,95 @@
-// Define colors for different Pokemon Types
+//Define colors for different Pokemon Types
 const typeColor = {
-  bug: "#26de81",
-  dragon: "#ffeaa7",
-  electric: "#fed330",
-  fairy: "#FF0069",
-  fighting: "#30336b",
-  fire: "#f0932b",
-  flying: "#81ecec",
-  grass: "#00b894",
-  ground: "#EFB549",
-  ghost: "#a55eea",
-  ice: "#74b9ff",
-  normal: "#95afc0",
-  poison: "#6c5ce7",
-  psychic: "#a29bfe",
-  rock: "#2d3436",
-  water: "#0190FF",
-};
-
-// URL for the Pokemon API
-const url = " https://pokeapi.co/api/v2/pokemon/";
-
-// Get References to HTML elements by their IDs
-const card = document.getElementById("card");
-const button = document.getElementById("button");
-
-// Function to fetch and display Pokemon data
-let getPokeData = () => {
-  let id = Math.floor(Math.random() * 150) + 1;
-  const finalUrl = url + id;
-  fetch(finalUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Failed to fetch data. Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      generateCard(data);
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
+    bug: "#26de81",
+    dragon: "#ffeaa7",
+    electric: "#fed330",
+    fairy: "#FF0069",
+    fighting: "#30336b",
+    fire: "#f0932b",
+    flying: "#81ecec",
+    grass: "#00b894",
+    ground: "#EFB549",
+    ghost: "#a55eea",
+    ice: "#74b9ff",
+    normal: "#95afc0",
+    poison: "#6c5ce7",
+    psychic: "#a29bfe",
+    rock: "#2d3436",
+    water: "#0190FF",
+  };
+  //URL for the Pokemon API
+  const url = " https://pokeapi.co/api/v2/pokemon/";
+  //Get References to HTML elements by their IDs
+  const card = document.getElementById("card");
+  const button = document.getElementById("button");
+  
+  //Function to fetch and display Pokemon data
+  let getPokeData = () => {
+    let id = Math.floor(Math.random() * 150) + 1;
+    const finalUrl = url + id;
+    fetch(finalUrl) //Fetch data from the PokeAPI
+      .then((response) => response.json()) //// Parse the response as JSON
+      .then((data) => {
+        generateCard(data);
+      });
+  };
+  
+ 
+  
+  let generateCard = (data) => { //Function to generate and display Pokemon card
+    
+    console.log(data);
+    const hp = data.stats[0].base_stat; //Extract data from API response
+    const imgSrc = data.sprites.other.dream_world.front_default;
+    const pokeName = data.name[0].toUpperCase() + data.name.slice(1); // Capitalize the first letter of the Pokemon
+    const statAttack = data.stats[1].base_stat;
+    const statDefense = data.stats[2].base_stat;
+    const statSpeed = data.stats[5].base_stat;
+  
+//Match the color for the card based on the Pokemon's type    
+    const themeColor = typeColor[data.types[0].type.name];
+    console.log(themeColor);
+    card.innerHTML = `
+          <p class="hp">
+            <span>HP</span>
+              ${hp}
+          </p>
+          <img src=${imgSrc} />
+          <h2 class="poke-name">${pokeName}</h2>
+          <div class="types">
+           
+          </div>
+          <div class="stats">
+            <div>
+              <h3>${statAttack}</h3>
+              <p>Attack</p>
+            </div>
+            <div>
+              <h3>${statDefense}</h3>
+              <p>Defense</p>
+            </div>
+            <div>
+              <h3>${statSpeed}</h3>
+              <p>Speed</p>
+            </div>
+          </div>
+    `;
+    appendTypes(data.types); // Add the Pokemon types
+    styleCard(themeColor);
+  };
+  let appendTypes = (types) => {
+    types.forEach((item) => {
+      let span = document.createElement("SPAN");
+      span.textContent = item.type.name;
+      document.querySelector(".types").appendChild(span);
     });
-};
-
-let generateCard = (data) => {
-  console.log(data);
-  const hp = data.stats[0].base_stat;
-  const imgSrc = data.sprites.other.dream_world.front_default;
-  const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
-  const statAttack = data.stats[1].base_stat;
-  const statDefense = data.stats[2].base_stat;
-  const statSpeed = data.stats[5].base_stat;
-
-  // Match the color for the card based on the Pokemon's type
-  const themeColor = typeColor[data.types[0].type.name];
-  console.log(themeColor);
-  card.innerHTML = `
-        <p class="hp">
-          <span>HP</span>
-            ${hp}
-        </p>
-        <img src=${imgSrc} />
-        <h2 class="poke-name">${pokeName}</h2>
-        <div class="types">
-         
-        </div>
-        <div class="stats">
-          <div>
-            <h3>${statAttack}</h3>
-            <p>Attack</p>
-          </div>
-          <div>
-            <h3>${statDefense}</h3>
-            <p>Defense</p>
-          </div>
-          <div>
-            <h3>${statSpeed}</h3>
-            <p>Speed</p>
-          </div>
-        </div>
-  `;
-  appendTypes(data.types);
-  styleCard(themeColor);
-};
-
-let appendTypes = (types) => {
-  types.forEach((item) => {
-    let span = document.createElement("SPAN");
-    span.textContent = item.type.name;
-    document.querySelector(".types").appendChild(span);
-  });
-};
-
-let styleCard = (color) => {
-  card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`;
-  card.querySelectorAll(".types span").forEach((typeColor) => {
-    typeColor.style.backgroundColor = color;
-  });
-};
-
-button.addEventListener("click", getPokeData);
-window.addEventListener("load", getPokeData);
+  };
+  let styleCard = (color) => {
+    card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`;
+    card.querySelectorAll(".types span").forEach((typeColor) => {
+      typeColor.style.backgroundColor = color;
+    });
+  };
+  
+  button.addEventListener("click", getPokeData);
+  window.addEventListener("load", getPokeData);
